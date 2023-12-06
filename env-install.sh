9fa52d398faad8ebe36011326d24fa4c4dd06784
Tobias Gasslander <tobias@gasslander.com>
2023-12-06 13:30:07 +0100

    fix i3 dependencies

--- env-install.sh
+++ env-install.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
 
+echo "apt upate"
+sudo apt update
+
 echo "Installing dev env"
 echo
+
+echo "Hack font"
+echo
+./fonts.inc
+
+echo "Powerlevel10k theme"
+echo
+source powerlevel10k.inc
+
+echo "i3 sources"
+echo
+source i3.inc
+
+sudo apt install -y \
+	zsh \
+	tmux \
+	stow \
+	feh \
+	curl \
+	clang \
+	htop \
+	i3-wm \
+	i3blocks \
+	i3lock \
+	dmenu
 
 echo "zsh"
 echo
@@ -11,25 +39,21 @@ else
 	echo "ZSH already installed"
 fi
 
-echo "Hack font"
-echo
-./fonts.inc
+git clone https://github.com/tgasslander/dotfiles.git ~/dotfiles
+cd ~/dotfiles
+stow i3 i3blocks nvim zsh p10k Xresources tmux
+cd -
 
 echo "oh-my-zsh"
 echo
 if [ ! -d "${HOME}/.oh-my-zsh" ]; then
 	source "oh-my-zsh.inc"
-else 
+else
 	echo "oh-my-zsh already installed"
 fi
 
-echo "Powerlevel10k theme"
+echo "nvim"
 echo
-source powerlevel10k.inc
+source nvim.inc
 
-sudo apt install -y \
-	tmux \
-	stow \
-	feh
 
-git clone https://github.com/tgasslander/dotfiles.git ~/dotfiles
